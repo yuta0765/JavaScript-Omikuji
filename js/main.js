d4fc188ef048e7beb7e4e866dc0ef42d5c05d1a6
yuta0765 <hopefulhopper123@gmail.com>
2020-03-21 17:14:58 +0900

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', () => {
+        const results = ['大吉', '中吉', '凶', '末吉'];
+        btn.textContent =results[Math.floor(Math.random() * results.length)];
+    });
+}
